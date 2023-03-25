f7277e24f162391469590260d7292c57352139b9
Sahilmehra23 <sahil@techis.io>
2023-03-25 14:45:58 +0530

    backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import products from '../products'
 import { Row ,Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 function HomeScreen() {
+  const[products,setProducts] = useState([])
+  useEffect(()=>{
+    async function fetchProducts(){
+    const{data}= await axios.get('http://127.0.0.1:8000/api/products/')
+    setProducts(data)
+    }
+    fetchProducts()
+  },[])
   return (
     <div>
         <Row>
