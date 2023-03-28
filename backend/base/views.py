379d3822cba7fd6fa2ef9b22f5c7d3889b356f60
mehraasahil <mehra3249@gmail.com>
2023-03-29 00:45:11 +0530

    backenddone

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
 
 @api_view(['GET'])
@@ -23,16 +25,29 @@ def getRoutes(request):
     ]
     return Response(routes)
 
+
 @api_view(['GET'])
 def getProducts(request):
-    product = None
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)#This line written after creat the serializer.py and products is the variable of queryset
+    return Response(serializer.data)
+
+# @api_view(['GET'])       # this function is used to show data from products.py in json format
+# def getProducts(request):
+#     product = None
+#     return Response(products)
+
+# @api_view(['GET'])           #this function is used to show product screen data when data is static came from file
+# def getProduct(request,pk):
+#     product = None
+#     for i in products:
+#         if i['_id'] == pk:
+#             product = i
+#             break
+#     return Response(product)
 
 @api_view(['GET'])
 def getProduct(request,pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
