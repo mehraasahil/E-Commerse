0668d69d90ce15b03cce022da1c686d6a4e5219d
mehraasahil <mehra3249@gmail.com>
2023-06-06 11:42:39 +0530

    added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('base.urls'))
+    #path('api/',include('base.urls')) this line changed into commment after creates diffrrent url folder and view
+
+    path('api/products/',include('base.urls.product_urls')),
+    path('api/users/',include('base.urls.user_urls')),
+    path('api/orders/',include('base.urls.order_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) #for static images data came from statci img folder
